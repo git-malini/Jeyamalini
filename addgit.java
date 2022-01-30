1e048b750c00072a52211dbc2642b80c80d7acb6
git-malini <98530477+git-malini@users.noreply.github.com>
2022-01-30 22:44:12 +0530

    Add files via upload

---
+++ addgit.java
@@ -0,0 +1,19 @@
+package additionsgit;
+
+public class addgit {
+
+	public static void main(String[] args) {
+		System.out.println("Enter any two numbers:");
+		int first=100;
+		int second=200;
+		System.out.println(first+","+second);
+		int sum=first+second;
+		System.out.println("the sum is"+sum);
+
+	}
+
+}
+
+
+	
+
